ba9d8a0d609be1fcc48752b0b2177dcc3c531ec7
wen952888 <wenxiu9528@outlook.com>
2025-04-29 15:31:11 +0800

    Create spades.py

---
+++ spades/spades.py
@@ -0,0 +1,8 @@
+class SpadesGame:
+    def __init__(self):
+        self.players_info = [
+            {"name": "玩家1", "avatar": "./static/avatar1.png", "id": 0, "hand": []},
+            {"name": "玩家2", "avatar": "./static/avatar2.png", "id": 1, "hand": []},
+            {"name": "玩家3", "avatar": "./static/avatar3.png", "id": 2, "hand": []},
+            {"name": "玩家4", "avatar": "./static/avatar4.png", "id": 3, "hand": []},
+        ]
